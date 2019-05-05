87b27653d770c759e238ba5c9c66375ae6a2cdfc
Shantanu Kandale <40430224+sgkandale@users.noreply.github.com>
2019-05-05 19:00:22 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,38 +1,8 @@
 <?php
 
 include('handlers/config.php');
+include('handlers/login_handler.php');
 
-$username = "";
-$password = "";
-$error_array = array();
-
-if(isset($_POST['log_submit']))
-{
-    $username = $_POST['log_username'];
-    $password = $_POST['log_password'];
-    
-    $_SESSION['log_username'] = $_POST['log_username'];
-    
-    $password = hash('sha512', $password);
-    
-    $check_details_query = mysqli_query($con, "SELECT * FROM net_users WHERE username='$username' AND password='$password'");
-    
-    if(mysqli_num_rows($check_details_query) == 1)
-    {
-        $_SESSION['username'] = $username;
-        header("Location: index.php");
-        exit();
-    }
-    else
-    {
-        array_push($error_array, "Incoreect Details");
-    }
-    
-    
-    $_SESSION['log_username'] = "";
-}
-
-?>
      
    
 <html>
